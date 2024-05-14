46a829634c9f6af0090b7da2550bbab6d67aa30e
asadjafferyy <asadjaf@uw.edu>
2024-05-14 14:12:23 -0700

    created data file

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header.js'; 
 import Footer from './Footer.js';
-import {CoursePage} from './CoursePage.js';
+import CoursePage from './CoursePage.js';
 
 function App() {
   return (
--- src/components/CoursePage.js
+++ src/components/CoursePage.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import coursesData from './courses.json'; // Import the course data
+import coursesData from '../data/courses.json'; // Import the course data
 import Course from './Course'; // Import the Course component
 import '../css/project-styling.css';
 
-function CoursePage({ code }) {
+export function CoursePage({ code }) {
     const course = coursesData.find(course => course.code === code);
 
     return (
