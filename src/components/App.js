e418d76d3b4206b2bf963f8c3dae12e5cd6f1fff
mason-koh <masonkoh@uw.edu>
2024-05-16 12:11:15 -0700

    changes to course, coursepage, and courses

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,10 @@ function App() {
   return (
     <div className="container">
       <Header />
-        <CoursePage />
-       <Footer />
+      <CoursePage code="INFO 340" />
+      <Footer />
     </div>
-
+    
   );
 }
 
