40185c27d984f8bc85650a6ae35b1b838a9450a7
asadjafferyy <asadjaf@uw.edu>
2024-05-16 11:59:13 -0700

    testing search page - creating search component

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header.js'; 
 import Footer from './Footer.js';
 import CoursePage from './CoursePage.js';
+import Search from './Search.js';
+import '../css/project-styling.css';
 
 function App() {
   return (
     <div className="container">
       <Header />
-        <CoursePage />
+        <Search />
        <Footer />
     </div>
 
