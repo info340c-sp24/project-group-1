6d09427996bd2541d9d12ae60aa5c9da7204817b
ShammuMeyyappan <shammu53@uw.edu>
2024-05-16 12:45:41 -0700

    added react router dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Header from './Header.js'; 
 import Footer from './Footer.js';
 import Search from './Search.js';
